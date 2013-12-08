cc461c72d0b11e7123622ebb8f957c5406035aee
Irvyne <thibaud.bardin@gmail.com>
2013-12-08 20:33:49 +0100

    Update unit testing

---
+++ Tests/Controller/App/AppKernel.php
@@ -0,0 +1,30 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            // Dependencies
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            // My Bundle to test
+            new Irvyne\EmbedlyBundle\IrvyneEmbedlyBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        // We don't need that Environment stuff, just one config
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
